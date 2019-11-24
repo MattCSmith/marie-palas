c3435706736390d17cfb9fa97acc97e91023e4a2
Matt Smith <mrmcchow@gmail.com>
2019-11-24 22:31:30 +0000

    Fixes

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,9 +4,9 @@ import Helmet from "react-helmet"
 import { graphql } from "gatsby"
 
 import PageLayout from "../layouts/page/page.layout.jsx"
-import CategoriesContainer from "../components/Categories-Container/Categories-Container.component.jsx"
+
 import Hero from "../components/Hero/Hero.component"
-import CardComponent from "../components/Card/Card.component"
+
 import CardContainer from "../components/Card-Container/Card-container.component"
 
 class RootIndex extends React.Component {
